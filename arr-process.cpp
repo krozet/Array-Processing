c2fc45e0ced4054d465186c6198c2a11a4393140
krozet <keawa.rozet@yahoo.com>
2018-02-25 11:36:25 -0800

    KR02 - Algoritm highly adjusted, not using two pointers anymore. New alg involves iterating through the array, then removing each number afterwards, instead of doing it all at once. Remove still needs to be implemented.

--- arr-process.cpp
+++ arr-process.cpp
@@ -6,6 +6,7 @@ Integer Plot Function
 */
 
 #include <iostream>
+#include <algorithm>
 #include <stdio.h>
 #include <cstring>
 
@@ -13,8 +14,20 @@ void reduce(int* array, int size);
 
 int main(int argc, char const** argv) {
   int array[] = {9,1,1,6,7,1,2,3,3,5,6,6,6,6,7,9};
+  int size = sizeof(array)/sizeof(int);
 
-  reduce(array, sizeof(array)/sizeof(int));
+  std::cout << "\nBefore reduction: ";
+  for (int i = 0; i < size; i++)
+    std::cout << array[i] << " ";
+
+  reduce(array, size);
+
+  size = sizeof(array)/sizeof(int);
+  std::cout << "\nAfter reduction: ";
+  for (int i = 0; i < size; i++)
+    std::cout << array[i] << " ";
+  std::cout << "\n";
+
   return 0;
 }
 
@@ -22,52 +35,24 @@ void reduce(int* array, int size) {
   int min = 0;
   int mid = 0;
   int max = 0;
-  int end;
-  bool erase;
 
-  for (int start = 0; start < size; start++) {
-    end = size-(start+1);
-    erase = (start >= end);
-
-    printf("Start: %d\tEnd: %d\n", array[start], array[end]);
-
-    if (array[start] > min) {
-      if (array[start] > mid) {
-        if (array[start] > max) {
-          if (erase) {
-            
-          }
-          //number is larger than max, so record the value
-           else {
-            min = mid;
-            mid = max;
-            max = array[start];
-          }
-        }
-        if (erase) {
-
-        }
-        //number is larger than mid, so record the value
-        else {
-          min = mid;
-          mid = array[start];
-        }
-      }
-      if (erase) {
-
-      }
-      //number is larger than min, so record the value
-      else {
-        min = array[start];
-      }
+  for (int i = 0; i < size; i++) {
+    if (array[i] > max) {
+      min = mid;
+      mid = max;
+      max = array[i];
     }
-    if (array[end] > min) {
-      if (array[end] > mid) {
-        if (array[end] > max) {
-          // max
-        }
-      }
+    else if (array[i] > mid && array[i] < max) {
+      min = mid;
+      mid = array[i];
     }
-
+    else if (array[i] > min && array[i] < mid) {
+      min = array[i];
+    }
   }
+
+  printf("\nMin: %d\tMid: %d\tMax: %d\n", min, mid, max);
+  array = std::remove(array, array + sizeof(array)/sizeof(int), min);
+  array = std::remove(array, array + sizeof(array)/sizeof(int), mid);
+  array = std::remove(array, array + sizeof(array)/sizeof(int), max);
 }
